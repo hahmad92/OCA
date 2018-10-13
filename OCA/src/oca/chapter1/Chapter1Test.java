0258ff3b7645c3f66136aa62afab5c22ad26237d
hahmad01 <hammadawan50@yahoo.com>
2018-10-13 21:42:51 +0500

    First Chapter added

--- OCA/src/oca/chapter1/Chapter1Test.java
+++ OCA/src/oca/chapter1/Chapter1Test.java
@@ -3,8 +3,8 @@ package oca.chapter1;
 public class Chapter1Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hammad Awan");
 	}
 
 }
+
