526dffbde5bcf6278bd2710a920259ce645c883e
hahmad01 <hammadawan50@yahoo.com>
2018-11-12 20:45:07 +0500

    numeric promotion examples added

--- OCA/src/oca/chapter2/Chapter2Test.java
+++ OCA/src/oca/chapter2/Chapter2Test.java
@@ -3,8 +3,9 @@ package oca.chapter2;
 public class Chapter2Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int y = 4;
+		double x = 3 + 2 * --y;
+		System.out.println(x);
 	}
 
 }
---
+++ OCA/src/oca/chapter2/NumericPromotion.java
@@ -0,0 +1,13 @@
+package oca.chapter2;
+
+import java.awt.Window.Type;
+
+public class NumericPromotion {
+
+	public static void main(String[] args) {
+		int x = 1;
+		long y = 33;
+		System.out.println();
+	}
+
+}
